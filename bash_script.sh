6f91093f1545d73aba7a1d03f1b0be1d5648912c
Garibodi <68135909+Garibodi@users.noreply.github.com>
2022-10-15 22:39:08 -0300

    Add files via upload

---
+++ bash_script.sh
@@ -0,0 +1,5 @@
+pwd # mostra onde você está
+cd / #move para a home
+ls -F #print uma lista dos arquivos com detalhes
+ls -F > lista_HOME.txt # envia a lista para um arquivo chamado lista_HOME.txt
+date +%D
